ccb6eb5a1a36f8f9f2aafe9997e8692406e8e037
shall24306-cloud <shall24306@gmail.com>
2026-03-18 04:17:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function goFullScreen() {
+  let elem = document.documentElement;
+
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+}
+
+function closePopup() {
+  document.querySelector(".overlay").style.display = "none";
+}
